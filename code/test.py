83a96621704ff5ec9cd50f5c21caffc40759ada4
Jonathan Sada <jonathansada@gmail.com>
2025-06-24 15:10:58 +0200

    Moved steps to a readme.md

--- code/test.py
+++ code/test.py
@@ -1,24 +1,3 @@
-# ======================
-# STEPS TO DO IN TERMINAL
-# =======================
-# 1- Create a new environment
-# python -m venv venv
-#
-# 2 - Git ignore venv folder
-# echo "venv" > .gitignore
-#
-# 3 - Activate Environment
-# Linux: . ./venv/bin/activate
-#
-# 4 - Install dependencies (if you forked the repo jump to 6)
-# python -m pip install pandas
-#
-# 5 - Freeze dependencies
-# python -m pip freeze > requirements.txt
-#
-# 6 - Install dependencies from requirements.txt
-# python -m pip install -r requirements.txt
-
 import pandas as pd
 
 print("Hello World!")
