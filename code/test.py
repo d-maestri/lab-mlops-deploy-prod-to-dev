a3b6c78af2525567372d287295754eb2078d7b13
gfkpth <groeg3@web.de>
2025-06-24 15:13:19 +0200

    added a describe for our cherished dataset

--- code/test.py
+++ code/test.py
@@ -29,3 +29,7 @@ housingdat = pd.read_csv('../data/king_county_houses_aa.csv')
 print(housingdat.head())
 print("Hello World!")
 
+
+print("Now that I have direct access, I can just do what I want, harharharhar")
+
+print(housingdat.describe())
