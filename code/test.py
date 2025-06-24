a25c57635e079c635366dd037e63912bd8ebdd1a
Jonathan Sada <jonathansada@gmail.com>
2025-06-24 14:38:20 +0200

    Created and environment and added gitignore

--- code/test.py
+++ code/test.py
@@ -1,1 +1,11 @@
+# ======================
+# STEPS TO DO IN TERMINAL
+# =======================
+# 1- Create a new environment
+# python -m venv venv
+#
+# 2 - Git ignore venv folder
+# echo "venv" > .gitignore
+#
+
 print("Hello World!")
