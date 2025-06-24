aa3f4a1dc0e1b0ac43cbc79735a3c36cfa64c67a
gfkpth <groeg3@web.de>
2025-06-24 15:21:50 +0200

    fixing the dataset description with an f-string

--- code/test.py
+++ code/test.py
@@ -12,3 +12,4 @@ print("Hello World!")
 print("Now that I have direct access, I can just do what I want, harharharhar")
 
 print("Check the dataset", housingdat.head(10))
+print(f'Actually this is better done with an f-string: {housingdat.describe()}')
