4756486c1ffe5db4a55a8216b958710b2f43c22a
gfkpth <groeg3@web.de>
2025-06-24 14:45:48 +0200

    making sure pandas are present and accounted for

--- code/test.py
+++ code/test.py
@@ -8,4 +8,11 @@
 # echo "venv" > .gitignore
 #
 
+import pandas as pd
+
 print("Hello World!")
+print("The world needs pandas!")
+
+housingdat = pd.read_csv('../data/king_county_houses_aa.csv')
+
+housingdat.head()
